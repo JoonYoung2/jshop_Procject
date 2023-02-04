a01c00ae2f1256b634a46d632b00231325a94561
codigzzang123 <hosun9752@gmail.com>
2023-02-04 16:29:01 +0900

    ADD : .gitignore

---
+++ src/main/java/com/project/jshop/sample/SampleTestController.java
@@ -0,0 +1,14 @@
+package com.project.jshop.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleTestController {
+
+    @RequestMapping("/sample")
+    public @ResponseBody String sample(){
+        return "sample";
+    }
+}
